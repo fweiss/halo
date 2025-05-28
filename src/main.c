17939d635110bf96e23b1513f978081888fadc93
Frank E Weiss <feweiss@gmail.com>
2025-05-28 15:00:00 -0700

    clean up code

--- src/main.c
+++ src/main.c
@@ -15,7 +15,8 @@
 #define GPIOB_ODR          (*(volatile unsigned int *)(GPIOB_BASE + 0x14))
 
 #define LED_PORT              GPIOB
-#define LED_PORT_RCC_EN       0x2u 
+#define GPIOBEN       0x2u
+#define GPIOCEN       0x4u
 #define GPIO_MODE_MASK       ~0x3u
 #define GPIO_MODE_OUTPUT      0x1u
 
@@ -24,49 +25,32 @@
 #define RED_LED_PIN           14u
 #define FLASH_LED             BLUE_LED_PIN
 
+int main(void) {
+    RCC_AHB1ENR |= GPIOBEN;
 
-void delayMs( unsigned delay );
+    GPIOB_MODER |= 0x4000;  // blue LED PB7
+    GPIOB_MODER |= 0x1;  // green LED PB0
+    GPIOB_MODER |= 0x1 << 28; // red LED PB14
 
-int main(void)
-{
-    RCC_AHB1ENR |= LED_PORT_RCC_EN ; // enable for PORT B
-    //LED_PORT->MODER &= GPIO_MODE_MASK << FLASH_LED ;
-    //LED_PORT->MODER |= GPIO_MODE_OUTPUT << FLASH_LED ;
-      GPIOB_MODER |= 0x4000;  // blue LED PB7
-      GPIOB_MODER |= 0x1;  // green LED PB0
-      GPIOB_ODR |= 0x1; // green LED ODR
-      GPIOB_MODER |=  0x1 << 28; // red LED PB14
-      GPIOB_ODR |= 0x1 <<14; // RED LED ODR
-      //GPIOB_BSRR |= 0x4000; // 7th pin 
     // connected to pull up resistor -- when we press button , it goes low
         //B1 user button is connected to PC13 - i.e. blue button
       // Enable clock to PC13 i.e. Port C
-      RCC_AHB1ENR |=  0x1 << 2;
+      RCC_AHB1ENR |= GPIOCEN;
       //GPIOC_MODER |=     //Input mode PC13- 13th pin 0
        // by default pins are input only
       GPIOC_MODER |=  0;
 
-    for(;;) {
+    GPIOB_ODR |= 0x1 << GREEN_LED_PIN;
+    GPIOB_ODR |= 0x1 << RED_LED_PIN;
+
+    for (;;) {
         // pressed blue button
-        if( (GPIOC_IDR & (1<<13)) ) // PC13
-            {
-                GPIOB_ODR |= 0x1 << FLASH_LED ;
-            }
-            else
-            {
-                GPIOB_ODR &= ~(0x1 << FLASH_LED) ;
-            }
+        if ( (GPIOC_IDR & (1<<13)) ) {
+            GPIOB_ODR |= 0x1 << FLASH_LED ;
+        } else {
+            GPIOB_ODR &= ~(0x1 << FLASH_LED) ;
+        }
     }
 
     return 0 ;
-}
-
-void delayMs( unsigned delay )
-{
-    for( volatile unsigned d = delay; d > 0; d--)
-    {
-        for( volatile int i = 0; i < 3195; i++ )
-        {
-        }
-    }
 }
